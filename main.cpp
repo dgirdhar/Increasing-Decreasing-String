da83826482a2e25accbd1156dd846cdbc0131139
Dhiraj Girdhar <girdhar.dhiraj@gmail.com>
2020-07-07 10:18:15 -0400

    Update main.cpp
    Changed the loop size, but no difference in performance.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,19 @@ public:
     string sortString(string s) {
         string res = "";
         int arr[26] = {0};
+        int min = 26;
+        int max = 0;
+        int newMin = 0;
+        int newMax = 0;
         
         for (int i = 0; i < s.size(); ++i) {
             arr[s[i]-'a']++;    
+            if (s[i] - 'a' < min) {
+                min = s[i] - 'a';
+            }
+            if (s[i] - 'a' > max) {
+                max = s[i] - 'a';
+            }
         }
         
         bool elementFound = false;
@@ -13,28 +23,32 @@ public:
         
         do {
             elementFound = false;
-            
+            cout << min << " " << max << endl;
             if (isIncreasing) {
                 isIncreasing = false;
                 
-                for (int i = 0; i < 26; ++i) {
+                for (int i = min; i <= max; ++i) {
                     if (arr[i] > 0) {
                         elementFound = true;
                         res += 'a' + i;
                         arr[i]--;
+                        newMax = i;
                     }
                 }
+                max = newMax;
             }
             else {
                 isIncreasing = true;
                 
-                for (int i = 25; i >= 0; --i) {
+                for (int i = max; i >= min; --i) {
                     if (arr[i] > 0) {
                         elementFound = true;
                         res += 'a' + i;
                         arr[i]--;
+                        newMin = i;
                     }
-                }                
+                }
+                min = newMin;
             }
         } while(elementFound);
         
